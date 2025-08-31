d817d562ba02cb89fa32c9951310b36f00a79806
Yug-the-pirate-king <yugshah197@gmail.com>
2025-08-31 15:36:18 +0530

    Updated Armstrong programme

--- Loops/Assignment/Armstrong.cpp
+++ Loops/Assignment/Armstrong.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 int main() {
-    int n ;
+    int n = 371 ;
+    int num = n;
     int res = 0;
 
-    cout << "Enter the number : ";
-    cin >> n;
-    int i = n;
+    // cout << "Enter the number : ";
+    // cin >> n;
     
-    while(n > 0){
-        res += pow((n%10) , 3);
-        n /= 10;
+    while(num > 0){
+        int lastDigit = num % 10;
+        res += pow(lastDigit , 3);
+        num /= 10;
     }
 
-    if(res == i){
+    if(n == res){
         cout << "Is Armstrong";
     }else{
         cout << "Is Not Armstrong" ;
