476d0d9fc68628a890799be72247ac6237170649
Yug-the-pirate-king <yugshah197@gmail.com>
2025-09-02 19:24:37 +0530

    upadated inverted_rotated_half_pyramid

--- Patterns/inverted_rotated_half_pyramid.cpp
+++ Patterns/inverted_rotated_half_pyramid.cpp
@@ -7,10 +7,10 @@ int main() {
     for(int i = 1;i <= n;i++){
         for(int j = 1;j <= n-i;j++)
         {
-            cout << "  ";
+            cout << " ";
         }
         for(int j = 1;j <= i;j++){
-            cout << "* ";
+            cout << "*";
         }
         cout << endl;
     }
