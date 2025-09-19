fa545174290435ec8082c9418a84cc9c3b6b7cdd
Yug Shah <yugshah197@gmail.com>
2025-09-19 12:02:59 +0530

    modified BuyAndSell

--- Arrays/BuyAndSell.cpp
+++ Arrays/BuyAndSell.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 void maxProfit(int *prices ,int n){
-    int maxprofit = INT32_MIN;
-    for(int i = 0;i< n-1;i++){
-        for(int j = 0;j<i;j++){
-        maxprofit = max(maxprofit,(prices[j+1]-prices[j]));
-        }
+    int maxprofit = 0, BestBuy = prices[0];
+    for(int i = 1;i< n;i++){
+        maxprofit = max(maxprofit,prices[i]-BestBuy);
+        BestBuy = min(BestBuy,prices[i]);
     }
-    cout << maxprofit;
+
+    cout << "The max profit is of " << maxprofit;
+
 }
 
 int main() {
-    int prices[] = {7, 1, 5, 3, 6, 4};
+    int prices[] = {1, 2, 3, 4, 5, 6, 7};
     int days = sizeof(prices)/sizeof(int);
 
     maxProfit(prices , days);
