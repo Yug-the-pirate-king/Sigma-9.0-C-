5c2e3c889e9c89f03ca48ff28f7326d573145191
Yug Shah <yugshah197@gmail.com>
2025-09-23 20:12:27 +0530

    todo

--- Sorting_algorithms/Insertion_sort.cpp
+++ Sorting_algorithms/Insertion_sort.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+void insertion_sort(int *nums,int size){
+    for(int i =0;i<size;i++){
+        int curr = nums[i];
+    }
+}
+
 int main() {
-    
+    int nums[] = {5,4,1,2,3};
+    int size = sizeof(nums)/sizeof(int);
+
+    insertion_sort(nums,size);
     return 0;
 }
