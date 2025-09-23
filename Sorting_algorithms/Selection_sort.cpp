b37e78e7552fbf797bd5a64fece9e9a3e2f88573
Yug Shah <yugshah197@gmail.com>
2025-09-23 19:53:30 +0530

    added Selection_sort

---
+++ Sorting_algorithms/Selection_sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void selection_sort( int *nums, int size)
+{
+    for(int i = 0;i<size;i++){
+        int minIndex = i;
+        for(int j=i+1;j<size;j++){
+            if(nums[minIndex] > nums[j]){
+                minIndex = j;
+}
+     }
+     swap(nums[i],nums[minIndex]);
+    }
+    for(int i =0;i<size;i++){
+    cout << nums[i] << "\n";
+}
+}
+int main() {
+    int nums[] = {5,4,1,2,3};
+    int size = sizeof(nums)/sizeof(int);
+
+    selection_sort(nums,size);
+    return 0;
+}
