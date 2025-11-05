2ca36087361e510984fdbba6b31285e5b1ee59f9
Yug Shah <yugshah197@gmail.com>
2025-11-05 15:41:38 +0530

    05-11-25

--- Bit_Manipulation/Count_Set_bit.cpp
+++ Bit_Manipulation/Count_Set_bit.cpp
@@ -1,1 +1,22 @@
 #include "../myheaders.h"
+
+void Count_set_Bit(int num){
+    int count = 0;
+
+    while(num >0){
+        int lastbit = num & 1;
+        count += lastbit;
+        num = num >> 1;
+    }
+
+    cout << "The total number of bits is " << count;
+}
+
+int main(){
+ int num ;
+    cout <<"Enter the number : \n";
+    cin >> num;
+
+    Count_set_Bit(num);
+
+}
