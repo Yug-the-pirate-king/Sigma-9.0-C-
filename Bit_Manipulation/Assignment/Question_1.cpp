5ecb98b9217f2f265fed95c518a9c42c0de5f1f1
Yug Shah <yugshah197@gmail.com>
2025-11-06 22:14:52 +0530

    06-11-25

--- Bit_Manipulation/Assignment/Question_1.cpp
+++ Bit_Manipulation/Assignment/Question_1.cpp
@@ -1,9 +1,20 @@
-#include"../myheaders.h"
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
 
 void clear_Bit(int num,int i,int j){
+     int allOnes = ~0;                // 111111... in binary
+    int left = allOnes << (j + 1);   // 1110000... after j
+    int right = (1 << i) - 1;        // 000...0111 up to i-1
+    int mask = left | right;         // 111000...0111
+    int result = num & mask;         // clears bits i–j
 
-    in
-(n & ~(1 << i))
+    cout << "Number after clearing bits from " << i << " to " << j << " = " << result << endl;
 }
 
 int main(){
