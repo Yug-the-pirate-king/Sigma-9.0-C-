22b25801cec1a8fc0ab5c8d3114dd7386f43f0e7
Yug Shah <yugshah197@gmail.com>
2025-11-06 21:56:21 +0530

    06-11-25

---
+++ Bit_Manipulation/Assignment/Question_1.cpp
@@ -0,0 +1,20 @@
+#include"../myheaders.h"
+
+void clear_Bit(int num,int i,int j){
+
+    in
+(n & ~(1 << i))
+}
+
+int main(){
+    int num ,i,j;
+    cout << "Enter the number : ";
+    cin >> num;
+    cout << "Enter the number form : ";
+    cin >> i;
+    cout << "to ";
+    cin >> j;
+
+    clear_Bit(num , i ,j);
+
+}
