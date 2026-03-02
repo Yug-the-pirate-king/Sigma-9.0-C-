8caa32a73a7d367003e8280da7ed0320c1fe1bdc
Yug Shah <yugshah197@gmail.com>
2026-03-02 19:03:17 +0530

    recusrsion

---
+++ Recusrsion/lec1_recusrsion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// do not run this code
+// Recursion function
+void function(){
+    cout<< "Funtion call ..";
+    function();
+}
+// do not run this code
+
+    int main()
+{
+    function();
+    return 0;
+}
+
+// do not run this code
