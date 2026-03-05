dbe1b91d0e0d680d61c55278889133493974cbaf
Yug Shah <yugshah197@gmail.com>
2026-03-05 20:50:21 +0530

    update

--- Recusrsion/power.cpp
+++ Recusrsion/power.cpp
@@ -11,12 +11,25 @@ int power(int x, int n) // O(n)
     return x * power(x, n - 1);
 }
 
-int power
+int powerfast(int x,int n){ //O(logn)
+    if(n==0){
+        return 1;
+    }
+
+    int halfpow = powerfast(x,n/2);
+    int halfpowerSquare = halfpow * halfpow;
+
+    if(n % 2 != 0){
+        return x * halfpowerSquare;
+    }
+    return halfpowerSquare;
+}
 
 int main()
 {
 
     cout << power(5, 2) <<"\n";
+    cout << powerfast(5, 2) <<"\n";
 
     return 0;
 }
