3216a669cac3910b02bac220d1d2f4ae9fe3407c
Yug Shah <yugshah197@gmail.com>
2026-03-21 16:52:02 +0530

    21-03-206

--- Recusrsion/Binary_String.cpp
+++ Recusrsion/Binary_String.cpp
@@ -2,12 +2,29 @@
 
 //Print all Binary Strings of size N without any consecutive 1s
 
-String binary_String(int n , String ans){
+void binary_String(int n , int lastplace, string ans){
+
+    if(n == 0){
+        cout << ans << "\n";
+        return;
+    }
+
+    if(lastplace != 1){
+        binary_String(n-1,0,ans+'0');
+        binary_String(n-1,1,ans+'1');
+    }
+    else{
+        binary_String(n-1,0,ans+'0');
+    }
 
 }
 
 int main(){
 
+    int n = 3;
+    string ans = "";
 
-    return 0
+    binary_String(n,0,ans);
+
+    return 0;
 }
