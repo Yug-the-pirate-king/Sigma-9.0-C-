005757cfa54313aebe96cb0844f6b9b25337f915
Yug Shah <yugshah197@gmail.com>
2026-06-05 20:37:23 +0530

    Add push_back method to linked list implementation

--- Linked_list/linked_list.cpp
+++ Linked_list/linked_list.cpp
@@ -40,6 +40,18 @@ public:
             head = newNode;
         }
     }
+
+    void push_back(int val){
+        Node *newNode = new Node(val);
+
+        if(head == NULL){
+            head = tail = newNode;
+        }
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
 };
 
 // Push Front
@@ -52,6 +64,12 @@ int main()
     ll.push_front(1);
 
     // 1->2->3 
+
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+
+    // 1->2->3->4->5->6
 
     return 0;
 }
