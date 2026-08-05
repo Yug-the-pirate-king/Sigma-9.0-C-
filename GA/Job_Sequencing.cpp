6dfbd4980b30dcd226968a19e931c713e379441d
Yug Shah <yugshah197@gmail.com>
2026-08-05 22:16:56 +0530

    Refactor job sorting logic and update job data for improved clarity

--- GA/Job_Sequencing.cpp
+++ GA/Job_Sequencing.cpp
@@ -3,19 +3,18 @@
 #include <algorithm>
 using namespace std;
 
-bool compare(pair<int, int> p1, pair<int, int> p2)
-{
-    return p1.second >= p2.second;
-}
 int main()
 {
     vector<pair<int, int>> jobs;
     jobs.push_back(make_pair(4, 20));
     jobs.push_back(make_pair(1, 10));
     jobs.push_back(make_pair(1, 40));
-    jobs.push_back(make_pair(1, 30));
+    jobs.push_back(make_pair(1, 10));
 
-    sort(jobs.begin(), jobs.end(), compare);
+    sort(jobs.begin(), jobs.end(), [](pair<int, int> p1, pair<int, int> p2)
+{
+    return p1.second >= p2.second;
+} );
     int porfit = jobs[0].second;
     int currjobend = 1;
     for(int i= 0;i<jobs.size();i++){
