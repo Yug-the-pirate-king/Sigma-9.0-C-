7bb7d15436498c2a812c4fb89ce84f7de1cf09ce
Yug Shah <yugshah197@gmail.com>
2026-08-28 21:10:25 +0530

    ai-upgrade: improve Counting_sort.cpp

--- Sorting_algorithms/Counting_sort.cpp
+++ Sorting_algorithms/Counting_sort.cpp
@@ -1,35 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <utility>
+
 using namespace std;
 
-void counting_sort(int *nums, int n){
-    int freq[100000];
-    int minVal = INT16_MAX,maxVal = INT16_MIN;
-    for(int i=0;i<n;i++){
-        minVal = min(minVal,nums[i]);
-        maxVal = max(maxVal,nums[i]);
+pair<int, int> findMinMax(const int* nums, int n) {
+    if (n <= 0) {
+        return {0, 0};
     }
-
-    for(int i= 0;i<n;i++){
-        freq[nums[i]]++;
-    }
-
-    for(int i=minVal,j=0;i<=maxVal;i++){
-        while(freq[i] > 0){
-            nums[j++] = i;
-            freq[i]--;
+    int minVal = nums[0];
+    int maxVal = nums[0];
+    for (int i = 1; i < n; ++i) {
+        if (nums[i] < minVal) {
+            minVal = nums[i];
+        }
+        if (nums[i] > maxVal) {
+            maxVal = nums[i];
         }
     }
+    return {minVal, maxVal};
+}
 
-    // Print sorted array
-    for (int i = 0; i < n; i++) {
+void printArray(const int* nums, int n) {
+    for (int i = 0; i < n; ++i) {
         cout << nums[i] << " ";
     }
     cout << endl;
 }
 
+void counting_sort(int* nums, int n) {
+    if (n <= 0) {
+        return;
+    }
+
+    pair<int, int> bounds = findMinMax(nums, n);
+    int minVal = bounds.first;
+    int maxVal = bounds.second;
+
+    long long range = static_cast<long long>(maxVal) - static_cast<long long>(minVal) + 1;
+    vector<int> freq(static_cast<size_t>(range), 0);
+
+    for (int i = 0; i < n; ++i) {
+        ++freq[nums[i] - minVal];
+    }
+
+    for (int i = minVal, j = 0; i <= maxVal; ++i) {
+        int idx = i - minVal;
+        while (freq[idx] > 0) {
+            nums[j++] = i;
+            --freq[idx];
+        }
+    }
+
+    printArray(nums, n);
+}
+
 int main() {
-    int nums[] = {1,4,1,3,2,4,3,7};
-    int n = sizeof(nums)/sizeof(int);
-    counting_sort(nums,n);
+    int nums[] = {1, 4, 1, 3, 2, 4, 3, 7};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    counting_sort(nums, n);
     return 0;
 }
