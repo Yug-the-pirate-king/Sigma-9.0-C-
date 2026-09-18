91df074c502c5e719e3236cf5ab4ecfe4d0d949f
Yug Shah <yugshah197@gmail.com>
2026-09-18 13:58:55 +0530

    ai-upgrade: improve Practice3.cpp

--- OOPS/Practice3.cpp
+++ OOPS/Practice3.cpp
@@ -2,22 +2,33 @@
 using namespace std;
 
 class Base {
-    public:
-    virtual void print(){
+public:
+    // Virtual destructor ensures derived-class destructors are invoked
+    // when an object is deleted through a base-class pointer.
+    virtual ~Base() = default;
+
+    virtual void print() {
         cout << "Base\n";
     }
 };
 
 class Derived : public Base {
-    public:
-    void print() override{
+public:
+    // 'override' guarantees this function overrides a base-class virtual.
+    void print() override {
         cout << "Derived\n";
     }
 };
 
 int main() {
+    // Create a Derived object but refer to it through a Base pointer.
     Base* b = new Derived();
-    b->print(); 
-    delete b; //Derived 
+
+    // Dynamic dispatch calls Derived::print() at runtime.
+    b->print();
+
+    // Safe deletion through base pointer because Base's destructor is virtual.
+    delete b;
+
     return 0;
 }
